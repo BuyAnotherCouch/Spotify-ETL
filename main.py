13daf01a885ad7d4a291c91773f130f1c9392401
Jason Hall <jason_h@live.ca>
2021-06-22 21:17:22 -0400

    add main.py

---
+++ main.py
@@ -0,0 +1,83 @@
+from typing import Mapping
+from pandas.core.series import Series
+import sqlalchemy
+import pandas as pd
+from sqlalchemy.orm import sessionmaker
+import requests
+import json
+import datetime
+import sqlite3
+
+DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
+USER_ID = "level_bastard"
+TOKEN = "BQA6zNXxI7CT8JBWIzeuJV88LFCJGtcxZ0w-ulBLw88gmPjBo0NHjXKJYM49NwdB_2Qma1wIunOhWM08iinb352nJjpV0uj-hK3bGULtRycpMwnGvukeM3RdhluqwVeOF1_Re1KeHMb9J8FRO6rGsjRF"
+
+def check_data(df: pd.DataFrame) -> bool:
+    #Check if the dataframe is empty
+    if df.empty:
+        print("No songs downloaded. Finishing execution.")
+        return False
+
+    # Primary Key Check
+    if pd.Series(df["played_at"]).is_unique:
+        pass
+    else:
+        raise Exception("Duplicates Detected!")
+
+    # Check for nulls
+    if df.isnull().values.any():
+        raise Exception("Null value found")
+
+    # Check that all timestamps are from the previous day.
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    yesterday = yesterday.replace(hour=0, minute=0, second= 0, microsecond= 0)
+
+    timestamps = df["timestamp"].tolist()
+    for timestamp  in timestamps:
+        if datetime.datetime.strptime(timestamp, "%Y-%m-%d") != yesterday:
+            raise Exception("At least one of the returned songs does not come from within the last 24 hours")
+# Extract
+if __name__ == "__main__":
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {token}".format(token=TOKEN)
+    }
+
+    today = datetime.datetime.now()
+    yesterday = today - datetime.timedelta(days=1)
+    yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
+
+    r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time = yesterday_unix_timestamp), headers=headers)
+
+    data = r.json()
+
+    song_names = []
+    artist_names = []
+    played_at_list = []
+    timestamps = []
+    album_type = []
+    release_date = []
+
+    for song in data["items"]:
+        song_names.append(song["track"]["name"])
+        artist_names.append(song["track"]["album"]["artists"][0]["name"])
+        played_at_list.append(song["played_at"])
+        timestamps.append(song["played_at"][0:10])
+        album_type.append(song["track"]["album"]["album_type"])
+        release_date.append(song["track"]["album"]["release_date"])
+
+    song_dict = {
+        "song_name" : song_names,
+        "artist_name" : artist_names,
+        "played_at" : played_at_list,
+        "timestamp" : timestamps,
+        "album_type" : album_type,
+        "release_date" : release_date
+    }
+
+    song_df = pd.DataFrame(song_dict, columns= ["song_name", "artist_name", "played_at", "timestamp", "album_type", "release_date"])
+
+# Validate
+if check_data(song_df):
+    print("Data valid, proceed to Load stage")
